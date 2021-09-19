c00f5b813d86e62fbfdee88dd0a397ed53f5066f
motephyr <motephyr@gmail.com>
2021-09-19 12:38:12 +0800

    Set tailwind

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,10 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
+// Route.get('/', async ({ view }) => {
+//   return view.render('welcome')
+// })
+
 Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+  return view.render('index')
 })
