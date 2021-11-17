01cf3e41448c175091d608cbf04849762865799c
motephyr <motephyr@gmail.com>
2021-11-17 15:27:14 +0800

    fix upload date

--- app/Services/ImportService.ts
+++ app/Services/ImportService.ts
@@ -4,26 +4,23 @@ import Group from 'App/Models/Group'
 
 export default class ImportService {
   public static async ImportClassification(filelocation) {
-    Group.truncate(true)
-
-    Source.truncate(true)
     let newworkbook = new Excel.Workbook()
 
     let workbook = await newworkbook.csv.readFile(filelocation)
+    const row = workbook.getRow(2);
+    const date = new Date(Number(row.values[1]) * 1000);
+    const datestring = `${date.getFullYear()}${(date.getMonth() + 1)}${date.getDate()}`
+    await Source.query().where({ datestring }).delete()
+    await Group.query().where({ datestring }).delete()
 
-    let today = new Date()
-    let yyyy = today.getFullYear();
-    let mm = String(today.getMonth() + 1).padStart(2, '0');
-    let dd = String(today.getDate()).padStart(2, '0');
     workbook.eachRow(async (row, rowNumber) => {
       if (rowNumber >= 2) {
         let values = JSON.parse(JSON.stringify(row.values))
 
+        let idstring = values[2] //get cell and the row
+        let timestring = values[1]
         if (values[3] !== -1) {
-          let idstring = values[2] //get cell and the row
-          let datestring = yyyy + mm + dd
-          let timestring = values[1]
-          let action = values[4] ? values[4] : values[7]
+          let action = values[4]
           let space = values[5]
           let url = values[6]
 
@@ -35,6 +32,17 @@ export default class ImportService {
             action,
             space,
             url
+          }
+
+          await Source.create(inputsource)
+        } else {
+          let action = values[7]
+
+          let inputsource = {
+            idstring,
+            datestring,
+            timestring,
+            action,
           }
 
           await Source.create(inputsource)
