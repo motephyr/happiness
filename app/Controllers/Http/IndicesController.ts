f98efb3369b21718f8be5e1a0d10d1bb42a0efac
motephyr <motephyr@gmail.com>
2021-11-18 19:32:17 +0800

    Fix index page

--- app/Controllers/Http/IndicesController.ts
+++ app/Controllers/Http/IndicesController.ts
@@ -1,11 +1,34 @@
 // import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Source from 'App/Models/Source'
+import Group from 'App/Models/Group'
 
 export default class IndicesController {
   public async index({ view }) {
 
-    let sourcesquery = await Source.query().distinct('datestring')
-    let sources = sourcesquery.map((source) => source.serialize())
-    return view.render('index', { sources })
+    let groupsquery = await Group.query()
+    let groups = groupsquery.map((group) => group.serialize())
+    // usage example:
+    let datestrings = groups.map((x) => (x.datestring)).filter(onlyUnique);
+    let times = datestrings.map((x) => {
+      return groups.filter((y) => {
+        return x === y.datestring
+      }).length
+    })
+
+    let longesttimes = datestrings.map((x) => {
+      let duringtimes = groups
+        .filter((y) => {
+          return x === y.datestring
+        }).map((z) => parseInt(z.duringtime))
+      return Math.max(...duringtimes)
+    })
+
+    let result = { datestrings, times, longesttimes }
+
+    return view.render('index', result)
   }
 }
+
+function onlyUnique(value, index, self) {
+  return self.indexOf(value) === index;
+}
+
