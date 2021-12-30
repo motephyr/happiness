e103459c60d4421e59cff14d2b2bb6355753808d
yi-ju wu <motephyr@gmail.com>
2021-12-30 09:50:07 +0800

    Fix rangeBar chart

---
+++ database/migrations/1639745635917_events.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Events extends BaseSchema {
+  protected tableName = 'events'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.text('message')
+      table.timestamp('checked_at', { useTz: true })
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
